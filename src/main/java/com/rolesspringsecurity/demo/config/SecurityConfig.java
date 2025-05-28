13c83872669e62341597ca50169846527ec17566
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-05-28 11:24:31 -0300

    feat: security config

---
+++ src/main/java/com/rolesspringsecurity/demo/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.rolesspringsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChainConfig(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                            auth.requestMatchers("/secure/api2").authenticated();
+                            auth.anyRequest().authenticated();
+                        }
+                )
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .httpBasic()
+                .and()
+                .build();
+    }
+}
